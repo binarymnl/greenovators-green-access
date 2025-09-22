8e2d174f7cda44b210a58327dbae65629c4d52f6
Jayesh Vasoya <jayesh.vasoya@gantnerindia.com>
2025-09-22 23:03:38 +0530

    Chnage Website Title

--- dashboard-frontend/src/vite.config.ts
+++ dashboard-frontend/src/vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react() , tailwindcss()],
   server:{
-    port:3000
+    port:3000,
+    host:true,
+    allowedHosts:["greenovator.domucloud.top"]
   }
 })
